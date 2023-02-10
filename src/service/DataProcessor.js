359d122d3f90f21e06112705bcb81bf34deb1f07
RomanSavushkin <romarsa82@gmail.com>
2023-02-10 12:38:03 +0200

    first commit

---
+++ src/service/DataProcessor.js
@@ -0,0 +1,14 @@
+export class DataProcessor {
+    #url
+    constructor(url) {
+        this.url = url;
+    }
+    async getData(latitude, longitude) {
+        const responseFromServer =
+            await fetch(`${this.#url}&latitude=${latitude}&longitude=${longitude}`);
+        return responseFromServer.json();
+    }
+    getTemperatureData(city, startDate, endDate, hourFrom, hourTo) {
+        //TODO
+    }
+}
