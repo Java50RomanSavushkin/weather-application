b8d57cf9b7d8194c4a1d2da30fda6abc44fac7c6
RomanSavushkin <romarsa82@gmail.com>
2023-02-13 22:23:05 +0200

    HW-25

--- src/main.js
+++ src/main.js
@@ -4,15 +4,4 @@ import { weatherConfig } from "./config/weather-config.js";
 const url = weatherConfig.url;
 const citiesObj = weatherConfig.cities;
 const dataProcessor = new DataProcessor(url, citiesObj);
-dataProcessor.getTemperatureData("Rehovot");
-
-// async function getTemperatureData() {
-    
-// }
-// displayTemperatures();
-// async function displayTemperatures() {
-//     const data = await dataProcessor.getData(29.5577, 34.9519);//Eilat
-//     // console.log(data);
-//     console.log(data.hourly.temperature_2m)
-// }
-// displayTemperatures();
+dataProcessor.getTemperatureData("Rehovot","2023-02-17","2023-02-17","00:00","02:00");
--- src/service/DataProcessor.js
+++ src/service/DataProcessor.js
@@ -7,17 +7,36 @@ export class DataProcessor {
     }
     async getData(latitude, longitude) {
         const responseFromServer =
-            await fetch(`${this.#url} & latitude=${latitude} & longitude=${longitude}`);
+            await fetch(`${this.#url}&latitude=${latitude}&longitude=${longitude}`);
         return responseFromServer.json();
-
-
     }
-
     async getTemperatureData(city, startDate, endDate, hourFrom, hourTo) {
         if (city in this.#cities) {
             let res = this.getCoordinatesByCity(city)
             const getDataByCoordinate = await this.getData(res[0], res[1]);
-            return console.log(JSON.stringify(getDataByCoordinate));
+            const getDateAndTimeArr = getDataByCoordinate.hourly.time;
+            const temperatureArr = getDataByCoordinate.hourly.temperature_2m;
+            const timesArr = []
+            const datesArr = []
+            for (let dateTimeString of getDateAndTimeArr) {
+                const time = dateTimeString.split("T")[1];
+                const date = dateTimeString.split("T")[0];
+                timesArr.push(time);
+                datesArr.push(date);
+            }
+            const filteredData = getDateAndTimeArr.filter((datetime, index) => {
+                const currentDate = datesArr[index];
+                const currentTime = timesArr[index];
+                return (currentDate >= startDate && currentDate <= endDate) && (currentTime >= hourFrom && currentTime <= hourTo);
+            });
+            const filteredTemperatureData = filteredData.map((datetime, index) => {
+                return {
+                    date: datesArr[getDateAndTimeArr.indexOf(datetime)],
+                    hour: parseInt(timesArr[getDateAndTimeArr.indexOf(datetime)], 10),
+                    temperature: temperatureArr[index]
+                }
+            });
+            console.log(filteredTemperatureData);
         }
     }
     getCoordinatesByCity(city) {
@@ -26,5 +45,4 @@ export class DataProcessor {
         const longitude = coords.longitude;
         return [latitude, longitude];
     }
-
 }
